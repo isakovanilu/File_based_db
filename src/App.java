5da68bf7404108bd3da63c2e87769f35c099ae66
Nilu Isakova <niluisakova@gmail.com>
2024-09-30 19:31:52 -0500

    added entities and repisitory files

--- src/App.java
+++ src/App.java
@@ -7,6 +7,8 @@ public class App {
         IRepository<AcademyGroup> groupRepository = new AcademyGroupFileRepository("group.file");
         groupRepository.Add(new AcademyGroup(0, "Group 2024"));
         groupRepository.Add(new AcademyGroup(1, "Group 2025"));
+        groupRepository.Add(new AcademyGroup(2, "Group 2026"));
+        groupRepository.Add(new AcademyGroup(3, "Group 2027"));
 
         for(AcademyGroup group: groupRepository.GetAll()){
             System.out.println(group.getId() + ", " + group.getGroupName());
