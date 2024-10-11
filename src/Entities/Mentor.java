becf371f6135af7c1f1e8c20e8abd08a7153e481
Nilu Isakova <niluisakova@gmail.com>
2024-10-11 14:02:49 -0500

    added Mentor class

---
+++ src/Entities/Mentor.java
@@ -0,0 +1,35 @@
+package Entities;
+
+public class Mentor {
+    private int id;
+    private String name;
+    private int academyId;
+
+    public Mentor(int id, String name, int academyId){
+        this.id = id;
+        this.name = name;
+        this.academyId = academyId
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id=id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name=name;
+    }
+
+    public int getAcademyId() {
+        return academyId;
+    }
+    public void setAcademyId(int academyId) {
+        this.academyId=academyId;
+    }
+
+}
